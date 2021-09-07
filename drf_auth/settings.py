005a40436313e2f0322bb8fee4df7470976a73b6
MOH <mohammadnoormjk1998@gmail.com>
2021-09-07 19:37:52 +0300

    update

--- drf_auth/settings.py
+++ drf_auth/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework', # new
-
+    'corsheaders',
     # Local
     'sport',
 ]
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
